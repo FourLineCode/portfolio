5d906bf1f788c5572ecdea2277e0f0d97c5fe4cf
akmal <akmal@ubuntu.ubuntu-domain>
2021-05-04 21:08:05 +0600

    linux changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
 		<div className='flex justify-center w-screen min-h-screen'>
-			<div className='w-1/2 min-h-full bg-gray-800'>hey</div>
+			<div className='w-1/2 min-h-full bg-gray-800'>hello</div>
 		</div>
-	)
+	);
 }
